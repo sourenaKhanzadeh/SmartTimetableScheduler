eedf07ff45c8964f5021225a492f97ff9d09f21b
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-03-18 23:05:28 -0400

    Removed IMGUI

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,8 @@
 // Created by sourena khanzadeh on 2023-03-18.
 //
 #include <iostream>
-#include <QApplication>
-#include <QWidget>
 
 int main(int argc, char *argv[]){
     std::cout << "Hello, World!" << std::endl;
-    // draw a window
-    QApplication app(argc, argv);
-    QWidget window;
-    window.resize(250, 150);
-    window.setWindowTitle("Hello World");
-    window.show();
-    return app.exec();
+    return 0;
 }
