89190f8f1c95757ff924a804298e73180a5c262d
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-03-19 11:48:31 -0400

    elements deleted

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,9 @@
 //
 // Created by sourena khanzadeh on 2023-03-18.
 //
-#include <elements.hpp>
-#include <string>
+#include <iostream>
 
-using namespace cycfi::elements;
-
-// Main window background color
-auto constexpr bkd_color = rgba(35, 35, 37, 255);
-auto background = box(bkd_color);
-
-int main(int argc, char** argv)
-{
-    app _app(argc, argv, "Elements App", "org.cycfi.elements.app");
-    window _win(_app.name());
-    _win.on_close = [&_app]() { _app.stop(); };
-
-    view view_(_win);
-
-    view_.content(
-            background
-    );
-
-    _app.run();
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
