a5aa08f9fc9c3be7c073070d32bd8d4f2fad4f40
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-03-18 23:49:31 -0400

    elements project initialized

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 //
 // Created by sourena khanzadeh on 2023-03-18.
 //
-#include <iostream>
+#include <elements.hpp>
+#include <string>
 
-int main(int argc, char *argv[]){
-    std::cout << "Hello, World!" << std::endl;
+using namespace cycfi::elements;
+
+// Main window background color
+auto constexpr bkd_color = rgba(35, 35, 37, 255);
+auto background = box(bkd_color);
+
+int main(int argc, char** argv)
+{
+    app _app(argc, argv, "Elements App", "org.cycfi.elements.app");
+    window _win(_app.name());
+    _win.on_close = [&_app]() { _app.stop(); };
+
+    view view_(_win);
+
+    view_.content(
+            background
+    );
+
+    _app.run();
     return 0;
 }
